0bf4c0429591bd23f824c9dbc2f2aa1f8a0c3f50
程鹏 <chengpeng@adnonstop.com>
2017-04-22 14:12:36 +0800

    add misfloatingball
    Signed-off-by: 程鹏 <chengpeng@adnonstop.com>

---
+++ MISFloatingBall/MISFloatingBall.h
@@ -0,0 +1,13 @@
+//
+//  MISFloatingBall.h
+//  MISFloatingBall
+//
+//  Created by Mistletoe on 2017/4/22.
+//  Copyright © 2017年 Mistletoe. All rights reserved.
+//  悬浮球
+
+#import <UIKit/UIKit.h>
+
+@interface MISFloatingBall : UIWindow
+
+@end
---
+++ MISFloatingBall/MISFloatingBall.m
@@ -0,0 +1,21 @@
+//
+//  MISFloatingBall.m
+//  MISFloatingBall
+//
+//  Created by Mistletoe on 2017/4/22.
+//  Copyright © 2017年 Mistletoe. All rights reserved.
+//
+
+#import "MISFloatingBall.h"
+
+@implementation MISFloatingBall
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
